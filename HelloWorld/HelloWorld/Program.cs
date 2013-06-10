a5226f0d476a67933db9d3cbe1475950f802f835
Alex Frere <alex.frere@intergen.co.nz>
2013-06-11 08:58:35 +1200

    Initial Commit
    The first commit of the first project (seeing how things work)

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name.");
+            var name = Console.ReadLine();
+            Console.WriteLine("Hello World! It's a pleasure to meet you " + name);
+            Console.ReadLine();
+        }
+    }
+}
